de6a54319b092c4c9f2de631f7b30b7ec7550372
Sovan Mishra (EXT-Nokia) <sovan.mishra@nokia.com>
2025-06-17 18:04:22 +0530

    rgdded

--- rg.tf
+++ rg.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "alpha" {
   name     = "alpha_rg"
   location = "westus"
 }
+
+resource "azurerm_resource_group" "alpha1" {
+  name     = "alpha_rg1"
+  location = "westus"
+}
